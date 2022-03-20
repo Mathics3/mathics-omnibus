7589268728856198ec736325aa97d933598c5197
rocky <rb@dustyfeet.com>
2022-03-20 17:53:03 -0400

    Correct BAT-file options

--- script/dmathicsscript.cmd
+++ script/dmathicsscript.cmd
@@ -1,3 +1,3 @@
 rem Simple Powershell command file to run mathicsscript
 rem add features of bash script
-docker run -it -rm --name mathics-cli --rm --tty --interactive --network=host mathicsorg/mathics:latest --mode cli
+docker run --name mathics-cli --rm --tty --interactive --network=host mathicsorg/mathics:latest --mode cli
