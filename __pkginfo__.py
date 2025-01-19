bca05eca553e8f3667ecdca189c91512f7eec05b
rocky <rb@dustyfeet.com>
2025-01-19 13:59:28 -0500

    Revise Python setup.py side

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -5,6 +5,7 @@ author = "Rocky Bernstein"
 author_email = "rb@dustyfeet.com"
 scripts = [
     "script/dmathics",
+    "script/dmathics3-tokens",
     "script/dmathicsdoc",
     "script/dmathicsdoccopy",
     "script/dmathicsserver",
@@ -28,36 +29,35 @@ long_description = read("README.rst") + "\n"
 
 # Setup in EXTRAS_REQUIRE various install options:
 #  mathicsscript_full, etc.
-django_full = {"ujson",}
-mathics_core_full = {"psutil", "scikit-image", "lxml", "wordcloud", "cython",}
-mathicsscript_full = {"PyYAML", "PyQT5", "cairosvg", "ujson",}
-pymathics_full = {"pymathics-natlang >= 6.2.0", "pymathics-graph >= 6.2.0",}
-full = mathicsscript_full | django_full | pymathics_full
+django_full = {"ujson"}
+mathics_core_full = {"psutil", "scikit-image", "lxml", "wordcloud", "cython"}
+mathicsscript_full = {"PyYAML", "PyQT5", "cairosvg", "ujson"}
+full = mathicsscript_full | django_full
 
 EXTRAS_REQUIRE = {}
-for field in "mathics_core_full mathicsscript_full pymathics_full full".split():
+for field in "mathics_core_full mathicsscript_full".split():
     EXTRAS_REQUIRE[field] = locals()[field]
 
 IS_PYPY = "__pypy__" in sys.builtin_module_names
 
 install_requires = [
-    "Mathics3 >= 7.0.0.dev0,<7.1.0",
-    "mathicsscript >= 6.2.0",
-    "Mathics-Django >= 6.2.0",
-    "pymathics-natlang >= 6.2.0",
-    "pymathics-graph >= 6.2.0",
+    "Mathics3 > 7.0.0",
+    "mathicsscript >= 7.0.0",
+    "Mathics-Django >= 7.0.0",
+    "pymathics-natlang >= 7.0.0",
+    "pymathics-graph >= 7.0.0",
 ]
 
-packages=["mathics_omnibus", "script"]
+packages = ["mathics_omnibus", "script"]
 py_modules = None
-short_desc = "A Collection Mathics components to provide the full Mathics experience"
+short_desc = "A Collection Mathics components to provide the full Mathics3 experience"
 url = "http://github.com/Mathics3/mathics-omnibus"
 
 classifiers = [
     "Operating System :: OS Independent",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
 ]
